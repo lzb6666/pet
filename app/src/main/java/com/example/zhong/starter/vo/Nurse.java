57dd485fd976006f9a613241be85e8ebe5cbf04c
lzb6666 <zhongboge@foxmail.com>
2018-11-26 18:51:01 +0800

    Create Nurse.java

---
+++ app/src/main/java/com/example/zhong/starter/vo/Nurse.java
@@ -0,0 +1,129 @@
+package com.example.zhong.starter.vo;
+
+/**
+ * Created by zhong on 2018/11/26 18:11
+ */
+public class Nurse {
+    private String nurseID;
+    private String announcer;
+    private String recipient;
+    private String recTime;
+
+
+    private String imgURL;
+    private String variety;
+    private String sex;
+    private String age;
+    private String health;
+    private String note;
+
+    private String other;
+
+    private String status;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNurseID() {
+        return nurseID;
+    }
+
+    public void setNurseID(String nurseID) {
+        this.nurseID = nurseID;
+    }
+
+    public String getAnnouncer() {
+        return announcer;
+    }
+
+    public void setAnnouncer(String announcer) {
+        this.announcer = announcer;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getRecTime() {
+        return recTime;
+    }
+
+    public void setRecTime(String recTime) {
+        this.recTime = recTime;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+
+    public String getVariety() {
+        return variety;
+    }
+
+    public void setVariety(String variety) {
+        this.variety = variety;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getOther() {
+        return other;
+    }
+
+    public void setOther(String other) {
+        this.other = other;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
